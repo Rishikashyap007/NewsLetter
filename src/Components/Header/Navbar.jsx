8817a3c9b31e07e282f67aeb41ce001de8c5cc26
Rishi Kashyap <kashyap1strishi@gmail.com>
2024-03-19 17:47:42 +0530

    final commit

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -4,34 +4,25 @@ function Navbar(){
     return(
         <>
         <div className="navbarclass">
-<<<<<<< HEAD
+
             <div className="navbar-img1" >
             <img src="https://substackcdn.com/image/fetch/w_88,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F048641cb-ee19-431b-80cc-e15ed631dd2f_256x256.png" />
-=======
-            <div >
-            <img src="https://substackcdn.com/image/fetch/w_88,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F048641cb-ee19-431b-80cc-e15ed631dd2f_256x256.png" className="navbar-img1"/>
->>>>>>> f42cffc41de4fc6bc2cbe7064efc72847d5b7d2d
+
+            </div >
    
-            </div>
             <div className="navbar-img2">
             <img src="https://substackcdn.com/image/fetch/e_trim:10:white/e_trim:10:transparent/h_72,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F2adee35b-fe7c-4baf-8d41-62ce8b964572_1344x256.png" className="navbar-img2"/>
 
             </div>
             <div className="navbar-btnclass">
-<<<<<<< HEAD
                 <button className=" btn1"><i class="fa-solid fa-magnifying-glass"></i></button>
                 <button className=" btn1"><i class="fa-solid fa-arrow-up-from-bracket"></i></button>
                 <button className="navbarbtn">+ subscribe</button>
                 <button className="navbarbtn">Sign in</button>
-=======
-                <button className="navbarbtn btn1"><i class="fa-solid fa-magnifying-glass"></i></button>
-                <button className="navbarbtn btn1"><i class="fa-solid fa-arrow-up-from-bracket"></i></button>
-                <button className="navbarbtn btn2">+ subscribe</button>
-                <button className="navbarbtn btn3">Sign in</button>
->>>>>>> f42cffc41de4fc6bc2cbe7064efc72847d5b7d2d
             </div>
         </div>
-        <hr/><div className="links">
+        <hr/>
+        <div className="links">
         <div  className="navbar_link">Home</div>
         <div  className="navbar_link">Register for DD18</div>
         <div  className="navbar_link">Become a member</div>
@@ -41,26 +32,19 @@ function Navbar(){
         </div>
         <hr/>
          <div className="navbar_secondpart">
-<<<<<<< HEAD
              <div className="second-part-container">
              <div className="navbar_image" >
               <img src="https://substackcdn.com/image/fetch/w_848,h_477,c_fill,f_webp,q_auto:good,fl_progressive:steep,g_center/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F897f83a1-fe39-4ff1-9266-286c6d6b7386_1989x1421.png" ></img>
-=======
-            <div >
-              <img src="https://substackcdn.com/image/fetch/w_848,h_477,c_fill,f_webp,q_auto:good,fl_progressive:steep,g_center/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F897f83a1-fe39-4ff1-9266-286c6d6b7386_1989x1421.png" className="navbar_image"></img>
->>>>>>> f42cffc41de4fc6bc2cbe7064efc72847d5b7d2d
-            </div>
+             </div> 
             <div className="navbar_para">
                 <h1>How to really build a compounding career?</h1>
                 <p>An article you will bookmark</p>
                 <p className="navbar_para_lastline">Nov 28, 2023 . ABHISHEK FROM GROWTHX</p>
             </div>
-<<<<<<< HEAD
+
              </div>
-=======
->>>>>>> f42cffc41de4fc6bc2cbe7064efc72847d5b7d2d
             </div>       
-                  
+         
         </>
     )
 }
